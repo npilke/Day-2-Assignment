8d5e0787a8f8d9a2115de0ba8af28053d78da782
npilke <88544591+npilke@users.noreply.github.com>
2021-08-07 23:39:20 +0530

    Day 2 Assignment Submitted

---
+++ main (1).cpp
@@ -0,0 +1,12 @@
+Question 1
+Find the output of the following code snippet: 
+int i =0, j = 0;
+int *p = &i, *q = &j;
+q = p;
+*q = 2
+cout<<i<<" "<<j;
+
+Ans=As p is pointing towards the addresses of i and q is pointing towards addresses of j initially 
+then after the assignment of q=p now q starts pointing towards i and updates the value at i=0 to 2 
+so values at the output be :-
+2 0
